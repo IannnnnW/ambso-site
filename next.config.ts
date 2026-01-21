e35ffd34d3f758a2462a731e59c1c22389b1715e
Ian Wasukira <wasukira@Ians-MacBook-Pro.local>
2026-01-20 22:41:10 -0600

    Corrected image assets path

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
         port: '',
-        pathname: '/**',
+        pathname: '/images/**',
       },
     ],
   },
