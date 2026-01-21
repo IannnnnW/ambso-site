d0cfb43c8e8e89ba304279e0dde5087125fbd8e1
Ian Wasukira <wasukira@Ians-MacBook-Pro.local>
2026-01-20 22:19:54 -0600

    Working on deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/ambso-site',
+  assetPrefix: '/ambso-site',
   images: {
     unoptimized: true,
     remotePatterns: [
